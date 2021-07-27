0bebdf8d75625fc49e1ddaf9a978e9158e7907cc
jin <kimjin2468@gmail.com>
2021-07-27 20:38:57 +0900

    유튜브,리워드,프로모션 추가

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ searchInputEl.addEventListener('blur', function(){
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function() {
-  console.log(window.scrollY);
+  
   if(window.scrollY > 500) {    
     //배지 숨기기
     //gsap.to(요소,지속시간,옵션)
@@ -45,3 +45,69 @@ fadeEls.forEach(function(fadeEl, index) {
       opacity:1
     })
 });
+
+// new Swiper(선택자,옵션)
+new Swiper('.notice-line .swiper-container', {
+   direction: 'vertical',
+   autoplay:true,
+   loop:true
+});
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView : 3,
+  spaceBetween:10,
+  centeredSlides:true,
+  loop:true,
+  autoplay: {
+    delay: 3000,       
+    },
+    pagination: {
+      el: '.promotion .swiper-pagination',  //페이지 번호 요소서택자
+      clickable:true   //사용자의 페이지 번호 요소 제어가능 여부  
+    },
+    navigation: {
+      prevEl: '.promotion .swiper-prev',
+      nextEl: '.promotion .swiper-next'
+    }
+  });
+
+
+  const promotionEl = document.querySelector('.promotion');
+  const promotionToggleBtn = document.querySelector('.toggle-promotion');
+  let isHidePromotion = false;
+  promotionToggleBtn.addEventListener('click', function(){
+    isHidePromotion = !isHidePromotion;
+    if(isHidePromotion){
+      //숨김 처리
+      promotionEl.classList.add('hide');
+    }else{
+      //보임 처리
+      promotionEl.classList.remove('hide');
+    }
+  })
+
+  // 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+  function floatingObject(seletor, delay, size) {
+    //gsap.to(요소,시간,옵션)
+    gsap.to(
+      seletor,  //선택자
+      random(1.5, 2.5),  //애니메이션 동작시간
+      { //옵션
+        y: size,
+        repeat:-1,
+        yoyo:true,
+        ease:Power1.easeInOutk,
+        delay:random(0, delay)
+    });
+  }
+
+  floatingObject('.floating1', 1, 15);
+  floatingObject('.floating2', .5, 15);
+  floatingObject('.floating3', 1.5, 20);
+
